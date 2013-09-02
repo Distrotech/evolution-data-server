0a562dcf4f2f2248b199888db39e8571ad2e2ae1
Matthew Barnes <mbarnes@redhat.com>
2013-09-02 08:55:22 -0400

    Store QUOTAROOT information in CamelIMAPXMailbox.
    Keep QUOTAROOT information in the named CamelIMAPXMailbox instead of
    CamelIMAPXFolder.  Also remove CamelIMAPXFolder's "quota-root-names"
    property, since it's no longer used.
    
    Removed functions:
    
      camel_imapx_folder_dup_quota_root_names()
      camel_imapx_folder_set_quota_root_names()

--- camel/camel-imapx-folder.h
+++ camel/camel-imapx-folder.h
@@ -97,11 +97,6 @@ CamelIMAPXMailbox *
 		camel_imapx_folder_list_mailbox	(CamelIMAPXFolder *folder,
 						 GCancellable *cancellable,
 						 GError **error);
-gchar **	camel_imapx_folder_dup_quota_root_names
-						(CamelIMAPXFolder *folder);
-void		camel_imapx_folder_set_quota_root_names
-						(CamelIMAPXFolder *folder,
-						 const gchar **quota_root_names);
 void		camel_imapx_folder_add_move_to_real_junk
 						(CamelIMAPXFolder *folder,
 						 const gchar *message_uid);
